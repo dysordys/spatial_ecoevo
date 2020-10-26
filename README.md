301f7935807a0e62c4323c4fb5041f502f8b8d63
Gyorgy Barabas <dysordys@umich.edu>
2020-10-26 19:26:48 +0100

    Updated scripts after manuscript revision

--- README.md
+++ README.md
@@ -1,23 +1,25 @@
 Computer code for the manuscript "The importance of species interactions in spatially explicit eco-evolutionary community dynamics under climate change".
 
 * Operating systems: CentOS 7, Ubuntu 19.10, MacOS 10.13, Windows 10
-* Software dependencies: R 3.6.1 (software has been tested on R 3.6.1 and above)
+* Software dependencies: R 3.6.3 (software has been tested on R 3.6.3 and higher)
 * Required R packages:
-  - `deSolve` (for integrating differential equations)
-  - `tidyverse` (for efficient data manipulation and plotting)
+  - `deSolve`: Integrating differential equations
+  - `tidyverse`: Efficient data manipulation and plotting
+  - `Rcpp`: Importing and compiling functions written in C/C++
 * Required non-standard hardware: none
-* Typical install time on a normal desktop computer: no appreciable time if R is already installed. Otherwise, it is the installation time of R.
+* Typical installation time on a normal desktop computer: no appreciable time if R is already installed. Otherwise, it is the installation time of R and the above three packages.
 
-There are two R scripts in this repository:
+There are two R scripts and one C++ file in this repository:
 
 * `ecoevo_main.R`: This script runs the eco-evolutionary model. It can either be run directly from within R, or via the command prompt, by entering the following:
   `Rscript ecoevo.R [vbar] [dbar] [model] [replicate] [outfile]`
   The input parameters are:
-  - `[vbar]`: average genetic variance (in units of degrees Celsius squared)
-  - `[dbar]`: average dispersal distance (expressed as the fraction of the pole-to-equator distance, 10,000 km)
-  - `[model]`: either `baseline`, `trophic`, `Tdep`, or `Tdep_trophic`, for the four model variants (`baseline` is without trophic interactions or temperature-dependent competition; `trophic` is with trophic interactions but no temperature-dependent competition; `Tdep` is with temperature-dependent competition but no trophic interactions; and `Tdep_trophic` is with both temperature-dependent competition and trophic interactions)
-  - `[replicate]`: an integer, used to seed the random number generator with (so keeping `replicate` equal in two subsequent runs of the same model results in the exact same output)
-  - `[outfile]`: name (with path and extension) of file in which the generated data will be saved (e.g. `cc_data.csv`). The output is saved in csv format. If this argument is left blank, then the data are not saved, and instead a plot of the dynamics is created.
+  - `[vbar]`: Average genetic variance (in units of degrees Celsius squared)
+  - `[dbar]`: Average dispersal distance (expressed as the fraction of the pole-to-equator distance, 10,000 km)
+  - `[model]`: Either `baseline`, `trophic`, `Tdep`, or `Tdep_trophic`, for the four model variants (`baseline` is without trophic interactions or temperature-dependent competition; `trophic` is with trophic interactions but no temperature-dependent competition; `Tdep` is with temperature-dependent competition but no trophic interactions; and `Tdep_trophic` is with both temperature-dependent competition and trophic interactions)
+  - `[replicate]`: An integer, used to seed the random number generator with (so keeping `replicate` equal in two subsequent runs of the same model results in the exact same output)
+  - `[outfile]`: Name (with path and extension) of file in which the generated data will be saved (e.g. `cc_data.csv`). The output is saved in csv format. If this argument is left blank, then the data are not saved, and instead a plot of the system's time evolution is created.
   
-* `plotting_functions.R`: functions for visualizing model results and replicating the figures of the manuscript
+* `plotting_functions.R`: Functions for visualizing model results and replicating the manuscript's figures
 
+* `rhs_eval.cpp`: Routines for evaluating the right-hand side of the dynamical equations. They are written in C instead of R, because this is a major bottleneck in terms of speed.
--- ecoevo_main.R
+++ ecoevo_main.R
@@ -1,14 +1,15 @@
 # To run, either execute within R or enter at the command prompt ($):
 # $ Rscript ecoevo.R [vbar] [dbar] [model] [replicate] [outfile]
 
-require(deSolve) # for solving ordinary differential equations (ODEs)
-require(tidyverse) # for manipulating and visualizing data
+require(deSolve) # solving ordinary differential equations (ODEs)
+require(tidyverse) # manipulating and visualizing data
+require(Rcpp) # importing C functions
 
+sourceCpp("rhs_eval.cpp") # compile external C functions
 source("./plotting_functions.R") # various functions for plotting final data
 
 
 # ---------------------------- input parameters --------------------------------
-
 
 clargs <- commandArgs(trailingOnly=TRUE)
 if (length(clargs)>0) { # command-line arguments
@@ -22,7 +23,7 @@ if (length(clargs)>0) { # command-line arguments
   S <- 50 # fifty species per trophic level
   vbar <- 1e-1 # average genetic variance = 0.1 celsius squared
   dbar <- 1e-5 # average dispersal = 1e-5 (100 meters per year)
-  model <- "baseline" # baseline model (one trophic level, constant comp coeffs)
+  model <- "Tdep_trophic" # model with 2 trophic levels & T-dep. coeffs
   replicate <- 1 # replicate number = 1
   outfile <- "" # no output file; make plot instead
 }
@@ -30,27 +31,14 @@ if (length(clargs)>0) { # command-line arguments
 
 # --------------------------------functions ------------------------------------
 
-
-# apply smoothed step function to an arbitrary array n: n < 0 are set to 0,
-# 0 <= n <= 1 are set to 10*n^3-15*n^4+6*n^5, and n > 1 are set to 1
-smoothstep <- function(n) {
-  return(ifelse(n<1, (1*(n>0))*(n*n*n*(10+n*(-15+6*n))), 1))
-}
-
-# temperature as a function of space (x) and time (t), where x goes
-# from 0 (north pole) to 1 (equator)
-Temp <- function(x, t, t0, tE, Cmax, Cmin, Tmax, Tmin) {
-  return((Tmax-Tmin)*x+Tmin + ((Cmin-Cmax)*x+Cmax)*smoothstep((t-t0)/tE))
-}
-
 # return matrix W[i,j], which is nonzero if consumer i eats resource j;
 # SR is the number of resource, SC the number of consumer species
 generate_network <- function(SR, SC) {
   w <- matrix(0, SR+SC, SR+SC) # initialize adjacency matrix
   for (i in 1:SR) { # determine which resources each consumer eats: it must eat
-    indices <- sort(c(i, sample((1:SC)[-i], SR/2-1))) # the one with
+    indices <- sort(c(i, sample((1:SC)[-i], 4))) # the one with
     w[i+SR,indices] <- 1 # matching trait value, plus a fixed number of
-  } # randomly assigned ones (in this case, to make the connectance 1/2)
+  } # randomly assigned ones (in this case 4 more, for 5 resources per consumer)
   omega <- numeric(0) # initialize matrix of consumption efforts
   rsum <- rowSums(w) # omega[i,j] is the proportion of i's consumption rate,
   for (i in 1:(SR+SC)) omega <- cbind(omega, rsum) # targeted at consuming j
@@ -59,113 +47,44 @@ generate_network <- function(SR, SC) {
   return(W)
 }
 
-# type II functional response
-funcresp <- function(n, Th, arate, W) {
-  return(arate*(W%*%diag(n))/as.vector(1+arate*Th*(W%*%(n))))
-}
-
-# differential equations for the change in species densities and trait
-# means through time and over space
-eqs <- function(time, state, pars) {
-  # arrange species densities and traits into SxL matrices
-  n <- matrix(state[1:(pars$S*pars$L)], pars$S, pars$L)
-  m <- matrix(state[(pars$S*pars$L+1):(2*pars$S*pars$L)], pars$S, pars$L)
-  n[n<1e-6] <- 0 # threshold of extinction
-  # reserve memory for arrays
-  dndt <- matrix(0, pars$S, pars$L) # time derivative of density i in patch k
-  dmdt <- matrix(0, pars$S, pars$L) # time derivative of mean trait i in patch k
-  b <- matrix(0, pars$S, pars$L)
-  g <- matrix(0, pars$S, pars$L)
-  an <- matrix(0, pars$S, pars$L)
-  Fn <- matrix(0, pars$S, pars$L)
-  bn <- matrix(0, pars$S, pars$L)
-  mign <- matrix(0, pars$S, pars$L)
-  summig <- matrix(0, pars$S, pars$L)
-  mnmu <- matrix(0, pars$S, pars$L)
-  # calculate ingredient functions
-  q <- smoothstep(n/pars$nmin) # reduction of genetic variance at low abundances
-  h2 <- q*pars$vmat/(q*pars$vmat+pars$venv) # heritability (V_genetic / V_total)
-  w <- pars$bw-pars$aw*m # temperature tolerance widths
-  T <- Temp(seq(from=0, to=1, l=pars$L), time, pars$t0, pars$tE, pars$Cmax,
-            pars$Cmin, pars$Tmax, pars$Tmin) # temperature in each patch
-  for (i in 1:pars$S) {
-    mign[i,] <- pars$d[i]*as.numeric(pars$mig%*%n[i,]) # sum_l mig_ikl*n_il
-    summig[i,] <- pars$d[i]*as.numeric(rep(1, pars$L)%*%pars$mig) # sum_l mig_ilk
-    mnmu[i,] <- pars$d[i]*as.numeric(pars$mig%*%
-                                       (n[i,]*m[i,])) # sum_l mig_ikl*m_il*n_il
-  }
-  for (k in 1:pars$L) {
-    ef <- exp(-(T[k]-m[,k])^2/(2*((w[,k])^2+pars$s))) # exp. factor
-    b[,k] <- (pars$rho/sqrt((w[,k])^2+pars$s))*ef - pars$kappa
-    g[,k] <- (pars$rho*pars$s/(((w[,k])^2+pars$s)^(3/2)))*ef*(T[k]-m[,k])
-    F <- funcresp(n[,k], pars$Th, pars$arate, pars$W)
-    Fn[,k] <- rowSums(pars$eps*F) - as.vector(t(F)%*%n[,k])
-  }
-  if (model %in% c("Tdep", "Tdep_trophic")) {
-    # sum matrix of phenotypic variances
-    sv <- outer(pars$s[1:SR], pars$s[1:SR], FUN="+")
-    for (k in 1:pars$L) {
-      # difference matrix of trait means
-      dm <- outer(m[1:SR,k], m[1:SR,k], FUN="-")
-      an[,k] <- (-exp(-dm^2/(2*sv+pars$eta^2))*pars$eta/
-                   sqrt(2*sv+pars$eta^2))%*%n[1:SR,k] # sum_j alpha_ijk*n_jk
-      bn[,k] <- (-2*exp(-dm^2/(2*sv+pars$eta^2))*s[1:SR]*pars$eta*(-dm)/
-                   (2*sv+pars$eta^2)^(3/2))%*%n[1:SR,k] # sum_j beta_ijk*n_jk
-    }
-  } else {
-    for (k in 1:pars$L) an[,k] <- (-pars$a)%*%n[,k] # sum_j alpha_ijk*n_jk
-  }
-  ninv <- 1/(n+(1e-10)) # ninv = 1/n_ik (plus 1e-10 to avoid division by 0)
-  # set up equations
-  dndt <- (n*b+n*an+Fn)*smoothstep(n/(1e-6))+mign-n*summig
-  dmdt <- h2*(g+bn+ninv*(mnmu-m*mign))
-  # implement periodic boundaries
-  dndt[,1] <- dndt[,1]+pars$d*(pars$mig[1,2]*n[,2] - pars$mig[2,1]*n[,1])
-  dndt[,pars$L] <- dndt[,pars$L]+pars$d*
-    (pars$mig[pars$L,pars$L-1]*n[,pars$L-1]-
-       pars$mig[pars$L-1,pars$L]*n[,pars$L])
-  dmdt[,1] <- dmdt[,1]+h2[,1]*pars$d*pars$mig[1,2]*n[,2]*ninv[,1]*(m[,2]-m[,1])
-  dmdt[,pars$L] <- dmdt[,pars$L]+h2[,pars$L]*pars$d*pars$mig[pars$L,pars$L-1]*
-    n[,pars$L-1]*ninv[,pars$L]*(m[,pars$L-1]-m[,pars$L])
-  # return equations by first flattening them back into a single vector
-  return(list(c(as.numeric(dndt), as.numeric(dmdt))))
-}
-
 # put the results of the numerical integration into a tidy table
-organize_data <- function(out, times, pars) {
-  dat <- out %>% as.data.frame %>% as_tibble # convert to tibble
-  dat <- dat %>% filter(time %in% times) # only keep specified time points
+organize_data <- function(dat, times, pars) {
+  dat <- dat %>%
+    as.data.frame() %>% # convert to data frame (needed for next step)
+    as_tibble() %>% # convert to tibble (tidyverse's improved data frame)
+    filter(time %in% times) # only keep specified time points
   names(dat)[1] <- "time" # name the first column "time"
-  index <- 2 # keep track of which column we are naming
+  index <- 2 # keep track of which column we are naming with this counter
   for (k in 1:pars$L) {
-    for (i in 1:pars$S) { # name columns showing densities
+    for (i in 1:pars$S) { # name columns for densities
       names(dat)[index] <- paste0("n_", i, "_", k) # naming convention:
       index <- index + 1 # "type_species_patch" - type is either m (trait),
     } # or n (density)
   }
   for (k in 1:pars$L) {
-    for (i in 1:pars$S) { # name columns showing trait values
-      names(dat)[index] <- paste0("m_", i, "_", k) # same naming convention
+    for (i in 1:pars$S) { # name columns for trait values
+      names(dat)[index] <- paste0("m_", i, "_", k) # (same naming convention)
       index <- index + 1
     }
   }
   dat %>%
-    gather("variable", "v", 2:ncol(dat)) %>% # tidy up the table
+    # normalize table by collapsing columns into a key-value column pair
+    pivot_longer(cols=2:ncol(.), names_to="variable", values_to="v") %>%
     # split "variable" into value type (density or trait), species, and patch
     separate(variable, c("type", "species", "patch"), sep="_") %>%
     # convert species & patch from string ("1","2",...) to integer (1,2,...)
     mutate(species=as.integer(species), patch=as.integer(patch)) %>%
     # split trait and abundance values into two columns
-    spread(type, v) %>%
-    # trophic level (tl): species with index over SR are consumers ("C"),
+    pivot_wider(names_from="type", values_from="v") %>%
+    # trophic level (tl): species with index greater than SR are consumers ("C"),
     # the rest are resources ("R")
     mutate(tl=ifelse(species>SR, "C", "R")) %>%
-    return
+    # return tidy table
+    return()
 }
 
 
 # ------------------------------- parameters -----------------------------------
-
 
 # number of species and number of patches
 SR <- S # number of resource species
@@ -203,16 +122,17 @@ venv <- vbar # environmental variance
 vmat <- matrix(rep(v, L), S, L) # genetic variances at each patch
 s <- v + venv # species' total phenotypic variances
 eta <- 1 # competition width (centigrade; only for Tdep and Tdep_trophic)
-eps <- c(rep(0, SR), rep(0.05, SC)) # feeding efficiency of consumers
-nmin <- 1e-5 # genetic variance is reduced below this threshold density
-aw <- 0.1 # (negative) slope of trait-dependence of tol. width
-bw <- 4 # intercept of trait-dependence of tol. width
+eps <- c(rep(0, SR), rep(0.3, SC)) # feeding efficiency of consumers
+nmin <- 1e-5 # below this threshold density, genetic variances are reduced
+aw <- 0.1 # (negative) slope of trait-dependence of tolerance width
+bw <- 4 # intercept of trait-dependence of tolerance width
 Tmax <- 25.0 # initial mean temperature at equator
 Tmin <- -10.0 # initial mean temperature at poles
 Cmax <- 9.66 # projected temperature increase at poles
 Cmin <- 1.26 # projected temperature increase at equator
-tE <- 300.0 # saturation time for climate change
-t0 <- 1000.0 # time at which climate change starts
+tstart <- -4000 # starting time (relative to start of climate change at t = 0)
+tE <- 300.0 # time at which climate change stops (assuming it starts at t = 0)
+tend <- 2500 # time at which integration ends
 
 # dispersal matrix
 mig <- matrix(0, L, L) # initialize dispersal matrix
@@ -221,40 +141,52 @@ mig <- mig + t(mig) # nearest-neighbor patches
 
 # initial conditions
 ninit <- matrix(0, S, L) # reserve memory for initial densities
-# initial trait means
-muinit <- matrix(seq(Tmin, Tmax, l=SR), SR, L)
+muinit <- matrix(seq(Tmin, Tmax, l=SR), SR, L) # initial trait means
 # initial temperatures
-Tempinit <- Temp(seq(from=0, to=1, l=L), 0, t0, tE, Cmax, Cmin, Tmax, Tmin)
+Tempinit <- Temp(seq(from=0, to=1, l=L), 0, tE, Cmax, Cmin, Tmax, Tmin)
 for (i in 1:SR) ninit[i,] <- exp(-(muinit[i,1]-Tempinit)^2/(2*2^2))
 # initial traits and densities for consumers
 if (model %in% c("trophic", "Tdep_trophic")) {
   muinit <- rbind(muinit, matrix(seq(Tmin, Tmax, l=SC), SC, L))
   for (i in (SR+1):S) ninit[i,] <- exp(-(muinit[i,1]-Tempinit)^2/(2*2^2))
 }
+ic <- c(ninit, muinit) # merge initial conditions into a vector
 
-# coerce parameters into list
+# coerce parameters into a list
 pars <- list(SR=SR, SC=SC, S=S, L=L, rho=rho, kappa=kappa, a=a, eta=eta,
              eps=eps, W=W, venv=venv, vmat=vmat, s=s, nmin=nmin, aw=aw, bw=bw,
              Tmax=Tmax, Tmin=Tmin, Th=Th, arate=arate, Cmax=Cmax, Cmin=Cmin,
-             tE=tE, t0=t0, d=d, mig=mig, model=model)
+             tE=tE, d=d, mig=mig, model=model)
 
 
 # --------------------------- integrate ODEs -----------------------------------
 
+before_cc <- ode(y=ic, times=seq(tstart, 0, by=200), func=eqs, parms=pars,
+                 method="bdf_d") # integrate ODEs before climate change starts
+ic <- as.numeric(before_cc[nrow(before_cc),-1]) # final state -> new initial cond.
+before_cc <- before_cc %>% # put before-climate-change solution into tidy tibble:
+  organize_data(times=seq(from=tstart, to=0, by=200), pars=pars) %>%
+  filter(time!=0) # remove time point 0 (will be starting point of during_cc)
 
-# Define sampling points along the time axis
-tmax <- 3500 # time units to simulate for
-stepout <- 1 # spacing of temporal output
-time <- seq(0, tmax, by=stepout) # define time axis
-# Solve the system of ODEs
-dat <- ode(func=eqs, y=c(ninit,muinit), times=time, parms=pars, method="rk4") %>%
-  # put results in organized tibble
-  organize_data(times=seq(from=t0, to=tmax, by=100), pars=pars) %>%
-  # add replicate, genetic var., dispersal rate, and model as new columns
+during_cc <- ode(y=ic, times=seq(0, tE, by=0.5), func=eqs, parms=pars,
+                 method="rk4") # integrate from start to end of climate change
+ic <- as.numeric(during_cc[nrow(during_cc),-1]) # final state -> new initial cond.
+during_cc <- during_cc %>% # put during-climate-change solution into tidy tibble:
+  organize_data(times=seq(from=0, to=tE, by=100), pars=pars) %>%
+  filter(time!=tE) # remove time point tE (will be starting point of after_cc)
+
+after_cc <- ode(y=ic, times=seq(tE, tend, by=100), func=eqs, parms=pars,
+                method="bdf_d") %>% # integrate from end of climate change to end
+  # put after-climate-change solution into tidy tibble:
+  organize_data(times=seq(from=tE, to=tend, by=100), pars=pars)
+
+# merge data from before, during, and after climate change
+dat <- bind_rows(before_cc, during_cc, after_cc) %>%
+  # add replicate, genetic var., dispersal rate, and structure as new columns
   mutate(replicate=replicate, vbar=vbar, dbar=dbar, model=model) %>%
   # merge average genetic variance and dispersal into a single column
   mutate(parameterization=paste("V:", vbar, " d:", dbar)) %>%
-  # create regions:
+  # create regions
   mutate(region=case_when(
     (patch<=round(max(patch)/3))   ~ "polar", # top third of patches are "polar"
     (patch>=round(2*max(patch)/3)) ~ "tropical", # bottom third are "tropical"
@@ -263,10 +195,9 @@ dat <- ode(func=eqs, y=c(ninit,muinit), times=time, parms=pars, method="rk4") %>
 
 # --------------------------- generate output ----------------------------------
 
-
 if (outfile!="") { # if data file to save to was not specified as empty (""):
   write_csv(dat, path=outfile) # save data to specified file
 } else { # otherwise, create a plot:
   # replace function below with any function from "plotting_functions.R"
-  plot_timeseries(filter(dat, time %in% c(1000,1100,1200,1300,2500,3400,3500)))
+  plot_timeseries(dat %>% filter(time %in% c(tstart, 0, tE, tend)))
 }
